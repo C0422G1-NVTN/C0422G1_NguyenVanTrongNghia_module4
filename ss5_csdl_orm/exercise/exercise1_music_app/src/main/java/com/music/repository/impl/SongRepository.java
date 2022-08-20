d7e0114d8d66a59e34c8e788b488344ad5aac5b2
C0422G1-Nguyễn Văn Trọng Nghĩa <nghianf@gmail.com>
2022-08-20 18:53:21 +0700

    update

---
+++ ss5_csdl_orm/exercise/exercise1_music_app/src/main/java/com/music/repository/impl/SongRepository.java
@@ -0,0 +1,50 @@
+package com.music.repository.impl;
+
+import com.music.model.Song;
+import com.music.repository.ISongRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class SongRepository implements ISongRepository {
+
+    private List<Song> songs = new ArrayList<>();
+
+    @Override
+    public List<Song> findAll() {
+        TypedQuery<Song> query = BaseRepository.entityManager.createQuery("select s from Song s", Song.class);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public void save(Song song) {
+        EntityTransaction entityTransaction = BaseRepository.entityManager.getTransaction();
+
+        entityTransaction.begin();
+
+        BaseRepository.entityManager.persist(song);
+
+        entityTransaction.commit();
+    }
+
+    @Override
+    public Song findById(int id) {
+        return BaseRepository.entityManager.find(Song.class, id);
+    }
+
+    @Override
+    public void remove(Song song) {
+        EntityTransaction entityTransaction = BaseRepository.entityManager.getTransaction();
+
+        entityTransaction.begin();
+
+        BaseRepository.entityManager.remove(song);
+
+        entityTransaction.commit();
+    }
+}
