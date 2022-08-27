29494b27d40fb987fdd7ec6d370b7ae435758ce6
C0422G1-Nguyễn Văn Trọng Nghĩa <nghianf@gmail.com>
2022-08-27 16:33:19 +0700

    update name controller

--- case_study_module4/src/main/java/com/example/case_study_module4/controller/HomeController.java
+++ case_study_module4/src/main/java/com/example/case_study_module4/controller/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class Cac {
+public class HomeController {
     @GetMapping("")
     public String buoi(){
         return "home/home";
